52edfc8447f631d98168f2c57c75f69a0ae8eb83
jorgedclopes <jorgedclopes@gmail.com>
2020-04-04 23:16:01 +0100

    Added first project setup file.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ChessAnalytics',
+    version='0.0.1',
+    packages='',
+    url='',
+    license='',
+    maintainer='Jorge Lopes',
+    maintainer_email='jorgedclopes@gmail.com',
+    description='Insights about one\'s chess games.',
+    include_package_data=True
+)
