0c047df8c2a86e26510e86da88207c6a2d157917
jorgelopes <jorgedclopes@gmail.com>
2020-04-06 20:24:11 +0100

    Reset environment variables on setup. This also fixes all tests.

--- src/setupEnv.py
+++ src/setupEnv.py
@@ -5,11 +5,12 @@ from pathlib import Path
 
 
 def setup(path=None):
-    # TODO: what if there isn't any key? It returns None
+    env_var = 'lichess_token'
     if path is not None:
         path = os.path.join(path, '.env')
+    os.environ.clear()
     load_dotenv(dotenv_path=path, verbose=True)
-    token = os.getenv("lichess_token")
+    token = os.getenv(env_var)
     if token is None:
         warnings.warn("No token loaded.", ResourceWarning)
     return token
