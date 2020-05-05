d827381d6cca52b587b3f19aa012fbd2874d10bf
jorgedclopes <jorgedclopes@gmail.com>
2020-05-05 23:03:54 +0100

    Ignore imports on lint. Fail on non-perfect lint. Redo rated filter on load games. Fix tests.

--- tests/unit/test_get_game_ids.py
+++ tests/unit/test_get_game_ids.py
@@ -13,23 +13,6 @@ TIME_30MIN = 30 * 60 * 1000
 
 
 class TestDownload:
-
-    # def test_no_game_ids(self):
-    #     log = logging.getLogger('test_no_game_ids')
-    #     user = lichess.api.user('carequinha')
-    #     initial_time = user['createdAt']
-    #     final_time = initial_time + TIME_30MIN
-    #     log.debug('before function call')
-    #     output = download_games(name='carequinha',
-    #                             initial_time=initial_time,
-    #                             latest_time=final_time,
-    #                             is_rated=None)
-    #     log.debug('after function call')
-    #     # check the first 30 minutes
-    #     assert output == "From remote."
-    #     game_id_list = os.listdir('resources/PGN_database')
-    #     assert len(game_id_list) == 2
-
     def test_no_game_ids_with_particular_pref_type(self):
         log = logging.getLogger('test_no_game_ids')
         user = lichess.api.user('carequinha')
@@ -40,35 +23,25 @@ class TestDownload:
         download_games(name='carequinha',
                        db_dir=db_test_dir,
                        pref_type='blitz',
-                       initial_time=initial_time,
-                       latest_time=final_time,
-                       is_rated=None)
-        log.debug('after function call')
-
-        # check the first 30 minutes
+                       latest_time=final_time)
         game_id_list = os.listdir(db_test_dir)
         shutil.rmtree(db_test_dir)
         assert len(game_id_list) == 1
 
     def test_with_game_ids_with_particular_pref_type(self):
-        log = logging.getLogger('test_no_game_ids')
         user = lichess.api.user('carequinha')
         initial_time = user['createdAt']
         final_time = initial_time + TIME_30MIN
         db_test_dir = 'resources/PGN_database_test'
-        log.debug('before function call')
         download_games(name='carequinha',
                        db_dir=db_test_dir,
                        initial_time=initial_time,
-                       latest_time=final_time,
-                       is_rated=None)
-        log.debug('after function call')
+                       latest_time=final_time)
         with pytest.warns(ResourceWarning) as w:
             download_games(name='carequinha',
                            db_dir=db_test_dir,
                            initial_time=initial_time,
-                           latest_time=final_time,
-                           is_rated=None)
+                           latest_time=final_time)
             warnings.warn('PGN database already downloaded.',
                           ResourceWarning)
 
@@ -79,25 +52,20 @@ class TestDownload:
         assert game_id_list == ['OwUkcBo7.pgn', 'urC8tV4n.pgn']
 
     def test_initial_time_default_arguments(self):
-        log = logging.getLogger('test_no_game_ids')
         user = lichess.api.user('carequinha')
         initial_time = user['createdAt']
         final_time = initial_time + TIME_30MIN
 
-        log.debug('before function call')
         db_test_dir1 = 'resources/PGN_database_test1'
         download_games(name='carequinha',
                        db_dir=db_test_dir1,
                        initial_time=initial_time,
-                       latest_time=final_time,
-                       is_rated=None)
+                       latest_time=final_time)
 
         db_test_dir2 = 'resources/PGN_database_test2'
         download_games(name='carequinha',
                        db_dir=db_test_dir2,
-                       latest_time=initial_time + TIME_30MIN,
-                       is_rated=None)
-        log.debug('after function call')
+                       latest_time=initial_time + TIME_30MIN)
 
         expected = os.listdir(db_test_dir1)
         actual = os.listdir(db_test_dir2)
@@ -116,14 +84,12 @@ class TestDownload:
         download_games(name='carequinha',
                        db_dir=db_test_dir1,
                        initial_time=initial_time,
-                       latest_time=latest_time,
-                       is_rated=None)
+                       latest_time=latest_time)
 
         db_test_dir2 = 'resources/PGN_database_test2'
         download_games(name='carequinha',
                        db_dir=db_test_dir2,
-                       initial_time=initial_time,
-                       is_rated=None)
+                       initial_time=initial_time)
         log.debug('after function call')
 
         expected = os.listdir(db_test_dir1)
